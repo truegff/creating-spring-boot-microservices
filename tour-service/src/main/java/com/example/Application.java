be0098ce9e13edc1b048c4bad7193c65fc75236d
Alexander Dolidze <alexander.dolidze@gmail.com>
2024-07-08 00:41:16 +0000

    configured package component scan

--- tour-service/src/main/java/com/example/Application.java
+++ tour-service/src/main/java/com/example/Application.java
@@ -3,13 +3,19 @@ package com.example;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-import com.example.tourservice.SpringBeans;
+import com.example.tourservice.TourManagementService;
 import com.example.tourservice.TravelAgentService;
 
 public class Application {
 
     public static void main(String[] args) {
-        ApplicationContext context = new AnnotationConfigApplicationContext(SpringBeans.class);
+        ApplicationContext context = new AnnotationConfigApplicationContext(Application.class.getPackageName());
+        TourManagementService tms = context.getBean(TourManagementService.class);
+        tms.createTour(
+            "Zoo Tour",
+            100,
+            true
+        );
         TravelAgentService agent = context.getBean(TravelAgentService.class);
         System.out.println("\n******Explore California Tour Catalogue******");
         agent.displayTours();
